a20c0b17859ea2f2857aeb3d6f4c5b974e7334f2
oFx4 <kiahohoho@naver.com>
2019-02-24 00:18:26 +0900

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === '재') {
+    msg.delete(); 
+    
+    msg.channel.sendMessage('test1');
+    
+  
+});
+
+client.login(process.env.TOKEN);
